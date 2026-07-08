011cf6ac20fdad6041d2569f633f39231ef406ee
Julian Harbeck <harbeck@tu-berlin.de>
2026-07-08 18:25:52 +0200

    Make accessor and converters available in module

--- pandas_skyfield_extension/__init__.py
+++ pandas_skyfield_extension/__init__.py
@@ -3,20 +3,27 @@ from .options import config
 from .position_extension import (
     SkyfieldPositionDtype,
     SkyfieldPositionExtensionArray,
+    SkyfieldPositionSeriesAccessor,
 )
 
-from .sf_converters import to_sf_time
-
-from .utils import (
-    at,
-    sf_position_to_series
+from .sf_converters import (
+    to_sf_time,
+    to_sf_angle,
+    to_sf_distance,
+    to_sf_velocity,
 )
+
+from .utils import at, sf_position_to_series
 
 __all__: list[str] = [
     "SkyfieldPositionDtype",
     "SkyfieldPositionExtensionArray",
+    "SkyfieldPositionSeriesAccessor",
     "at",
     "config",
     "sf_position_to_series",
     "to_sf_time",
+    "to_sf_angle",
+    "to_sf_distance",
+    "to_sf_velocity",
 ]
