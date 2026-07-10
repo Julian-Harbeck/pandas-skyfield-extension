82723df3ceb3c270c9506b76654467eaa23a5f7c
Julian Harbeck <harbeck@tu-berlin.de>
2026-07-11 01:28:00 +0200

    Remove unused `_LENGTH_UNIT` and `_VELOCITY_UNIT` from `options.py`

--- pandas_skyfield_extension/options.py
+++ pandas_skyfield_extension/options.py
@@ -93,25 +93,3 @@ config: Config = Config(u.km, u.km / u.s, u.deg)
 Use this object to set the standard astropy units used to convert the Skyfield
 Distance, Velocity and Angle objects to astropy.
 """
-
-
-_LENGTH_UNIT: u.Unit = u.km
-_VELOCITY_UNIT: u.Unit = u.km / u.s
-
-
-def set_standard_length_unit(unit: u.Unit) -> None:
-    global _LENGTH_UNIT
-    _LENGTH_UNIT = unit
-
-
-def get_standard_length_unit() -> u.Unit:
-    return _LENGTH_UNIT
-
-
-def set_standard_velocity_unit(unit: u.Unit) -> None:
-    global _VELOCITY_UNIT
-    _VELOCITY_UNIT = unit
-
-
-def get_standard_velocity_unit() -> u.Unit:
-    return _VELOCITY_UNIT
